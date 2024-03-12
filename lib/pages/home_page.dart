be2291d9e4b7f465f2b694df6a89aaa8936b3d0f
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-12 19:02:49 +0300

    На экран home добавлена музыка

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_music_player/components/my_drawer.dart';
+import 'package:minimal_music_player/model/playlist_provider.dart';
+import 'package:minimal_music_player/model/song.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,6 +21,19 @@ class _HomePageState extends State<HomePage> {
         title: const Text("P L A Y L I S T"),
       ),
       drawer: const MyDrawer(),
+      body: Consumer<PalyListPorvider>(builder: (context, value, child) {
+        final List<Song> playlist = value.playlist;
+        return ListView.builder(
+            itemCount: playlist.length,
+            itemBuilder: ((context, index) {
+              final Song song = playlist[index];
+              return ListTile(
+                title: Text(song.songName),
+                subtitle: Text(song.artistName),
+                leading: Image.asset(song.albumArtImagePath),
+              );
+            }));
+      }),
     );
   }
 }
