7734b2e58a3f6eb102bffc1a21e7011b77416bb2
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-12 14:54:03 +0300

    MyDrawer добавлен на экран home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_music_player/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,6 +14,10 @@ class _HomePageState extends State<HomePage> {
     ThemeData theme = Theme.of(context);
     return Scaffold(
       backgroundColor: theme.colorScheme.background,
+      appBar: AppBar(
+        title: const Text("P L A Y L I S T"),
+      ),
+      drawer: const MyDrawer(),
     );
   }
 }
