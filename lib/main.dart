98cf3838fb6d98c14873ba080c4e0c13dd68369f
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-12 19:02:08 +0300

    Сделан провайдер для плейлистов
    + В main теперь MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:minimal_music_player/app/app.dart';
+import 'package:minimal_music_player/app/main_app.dart';
+import 'package:minimal_music_player/model/playlist_provider.dart';
 import 'package:minimal_music_player/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    child: const MainApp(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => PalyListPorvider()),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
---
+++ lib/model/playlist_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_music_player/model/song.dart';
+
+class PalyListPorvider extends ChangeNotifier {
+  final List<Song> _playlist = [
+    Song(
+      songName: "songName1",
+      artistName: "artistName1",
+      albumArtImagePath: "assets/images/album-1.jpg",
+      audioPath: "assets/audio/Colorful-Flowers.mp3",
+    ),
+    Song(
+      songName: "songName2",
+      artistName: "artistName2",
+      albumArtImagePath: "assets/images/album-2.jpeg",
+      audioPath: "assets/audio/Colorful-Flowers.mp3",
+    ),
+    Song(
+      songName: "songName3",
+      artistName: "artistName3",
+      albumArtImagePath: "assets/images/album-3.jpeg",
+      audioPath: "assets/audio/Colorful-Flowers.mp3",
+    ),
+  ];
+
+  int? _currentSongIndex;
+
+  List<Song> get playlist => _playlist;
+  int? get currentSongIndex => _currentSongIndex;
+}
