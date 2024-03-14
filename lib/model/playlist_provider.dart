580534974e5f61ca4a12322796376b0be9c1cfdd
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-14 11:33:10 +0300

    Реализован музыкальный плеер

--- lib/model/playlist_provider.dart
+++ lib/model/playlist_provider.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:minimal_music_player/model/song.dart';
 
@@ -7,29 +8,116 @@ class PalyListPorvider extends ChangeNotifier {
       songName: "songName1",
       artistName: "artistName1",
       albumArtImagePath: "assets/images/album-1.jpg",
-      audioPath: "assets/audio/Colorful-Flowers.mp3",
+      audioPath: "audio/Colorful-Flowers.mp3",
     ),
     Song(
       songName: "songName2",
       artistName: "artistName2",
       albumArtImagePath: "assets/images/album-2.jpeg",
-      audioPath: "assets/audio/Colorful-Flowers.mp3",
+      audioPath: "audio/Colorful-Flowers.mp3",
     ),
     Song(
       songName: "songName3",
       artistName: "artistName3",
       albumArtImagePath: "assets/images/album-3.jpeg",
-      audioPath: "assets/audio/Colorful-Flowers.mp3",
+      audioPath: "audio/Colorful-Flowers.mp3",
     ),
   ];
 
   int? _currentSongIndex;
 
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+
+  PalyListPorvider() {
+    listenToDuration();
+  }
+
+  bool _isPlaying = false;
+
+  Future<void> play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
+    await _audioPlayer.stop();
+    await _audioPlayer.play(AssetSource(path));
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  Future<void> pause() async {
+    await _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  Future<void> resume() async {
+    await _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  Future<void> pauseOrResume() async {
+    if (_isPlaying) {
+      pause();
+    } else {
+      resume();
+    }
+    notifyListeners();
+  }
+
+  Future<void> seek(Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  void playNextSong() {
+    if (_currentSongIndex != null) {
+      if (_currentSongIndex! < _playlist.length - 1) {
+        currentSongIndex = _currentSongIndex! + 1;
+      } else {
+        currentSongIndex = 0;
+      }
+    }
+  }
+
+  Future<void> playPreviousSong() async {
+    if (_currentDuration.inSeconds > 2) {
+    } else {
+      if (_currentSongIndex! > 0) {
+        currentSongIndex = _currentSongIndex! - 1;
+      } else {
+        currentSongIndex = _playlist.length - 1;
+      }
+    }
+  }
+
+  void listenToDuration() {
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPositionChanged.listen((newPosition) {
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      playNextSong();
+    });
+  }
+
   List<Song> get playlist => _playlist;
   int? get currentSongIndex => _currentSongIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
 
   set currentSongIndex(int? newIndex) {
     _currentSongIndex = newIndex;
+    if (newIndex != null) {
+      play();
+    }
     notifyListeners();
   }
 }
